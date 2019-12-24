67be9c16c80fb1c7405d2505dec04da8e4b729af
vidur.punj <vidur.punj@hotmail.com>
2019-12-24 18:06:25 +0530

    update

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,10 @@
 import React from 'react';
 const person = (props) =>  {
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>I am a {props.name}! and I am {props.age} years old</p>
             <p>{props.children}</p>
-            <input type="text" onChange={props.change}></input>
+            <input type="text" onChange={props.change} value= {props.name}></input>
         </div>
         ) 
 }; 
